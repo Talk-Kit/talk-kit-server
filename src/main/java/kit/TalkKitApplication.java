99f07faae2afe5af35b5ce6c0664b37f856d84d4
jickDo <pocard0117@naver.com>
2024-06-24 18:25:03 +0900

    featL: first commit

---
+++ src/main/java/kit/TalkKitApplication.java
@@ -0,0 +1,13 @@
+package kit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkKitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkKitApplication.class, args);
+	}
+
+}
