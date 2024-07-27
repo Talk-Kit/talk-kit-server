8de87393c3879d5388b655b0ca0f7c90299efcd7
jickDo <pocard0117@naver.com>
2024-07-27 22:32:36 +0900

    test: cd test

--- canal-api/src/main/java/com/canal/example/ExampleController.java
+++ canal-api/src/main/java/com/canal/example/ExampleController.java
@@ -1,6 +1,7 @@
 package com.canal.example;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +9,11 @@ public class ExampleController {
 
 	@GetMapping("/example")
 	public void exampleController() {
+		System.out.println("TEST");
+	}
+
+	@PostMapping("/example")
+	public void example2Controller() {
 		System.out.println("TEST");
 	}
 }
