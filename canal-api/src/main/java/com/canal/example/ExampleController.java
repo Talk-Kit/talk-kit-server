d211a2cfd729c56ff4cd6d2abaac95de4210799c
jickDo <pocard0117@naver.com>
2024-07-27 18:50:09 +0900

    feat: 테스트 api 생성

---
+++ canal-api/src/main/java/com/canal/example/ExampleController.java
@@ -0,0 +1,13 @@
+package com.canal.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleController {
+
+	@GetMapping("/example")
+	public void exampleController() {
+		System.out.println("TEST");
+	}
+}
