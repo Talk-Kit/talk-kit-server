fe8263ea4d52dbea3c3f0db54307aa8b6a887bfc
jickDo <pocard0117@naver.com>
2024-08-02 18:30:57 +0900

    test: 새로운 이미지 태그 받기위한 테스트4

--- canal-api/src/main/java/com/canal/example/ExampleController.java
+++ canal-api/src/main/java/com/canal/example/ExampleController.java
@@ -9,11 +9,11 @@ public class ExampleController {
 
 	@GetMapping("/example")
 	public String exampleController() {
-		return "TEST-Get3";
+		return "TEST-Get4";
 	}
 
 	@PostMapping("/example")
 	public String examplePostController() {
-		return "TEST-Post3";
+		return "TEST-Post4";
 	}
 }
