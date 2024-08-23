99b82fe1a175598a4f2997925dada03d71c3bf3a
jickDo <pocard0117@naver.com>
2024-08-23 15:41:05 +0900

    feat: api-gateway 테스트

--- canal-api/src/main/java/com/canal/CanalApiApplication.java
+++ canal-api/src/main/java/com/canal/CanalApiApplication.java
@@ -2,8 +2,10 @@ package com.canal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CanalApiApplication {
 
 	public static void main(String[] args) {
--- canal-api/src/main/java/com/canal/example/ExampleController.java
+++ canal-api/src/main/java/com/canal/example/ExampleController.java
@@ -2,9 +2,11 @@ package com.canal.example;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/members")
 public class ExampleController {
 
 	@GetMapping("/example")
