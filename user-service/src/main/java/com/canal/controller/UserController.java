bb1fc401fbdc0ec1e03113ba80eaff52c30012f7
yj_k <chloe_kang@naver.com>
2024-10-06 17:21:17 +0900

    Fix: swagger-user-service

--- user-service/src/main/java/com/canal/controller/UserController.java
+++ user-service/src/main/java/com/canal/controller/UserController.java
@@ -5,7 +5,6 @@ import com.canal.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
