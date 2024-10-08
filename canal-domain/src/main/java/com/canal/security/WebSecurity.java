dc0dae6b6461c3d7f17d5e5a4606f2b9a110dc17
yjk7890 <kyj01219@gmail.com>
2024-10-08 18:37:23 +0900

    Fix: cors 설정 변경

--- canal-domain/src/main/java/com/canal/security/WebSecurity.java
+++ canal-domain/src/main/java/com/canal/security/WebSecurity.java
@@ -12,6 +12,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 
 @Configuration
@@ -27,7 +32,7 @@ public class WebSecurity {
 
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
+                .cors(cors-> cors.configurationSource(cosConfig()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST,"/api/user-service/login").permitAll()
                         .requestMatchers(HttpMethod.POST,"/api/user-service/email/**").permitAll()
@@ -51,6 +56,19 @@ public class WebSecurity {
     @Bean
     public AuthenticationManager authManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    public CorsConfigurationSource cosConfig() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // 허용할 Origin 설정
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 메서드 설정
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept", "X-Requested-With")); // 허용할 헤더 설정
+        configuration.setAllowCredentials(true); // 인증 정보 허용 여부 설정 (예: 쿠키)
+        configuration.setMaxAge(3600L); // preflight 요청의 캐시 시간 설정 (초 단위)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 CORS 설정 적용
+        return source;
     }
 
 }
